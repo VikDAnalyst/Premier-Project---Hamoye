c1a57635866d4aff36405c36f86c2400d9e722df
Amaefula Maryann <amaannmary006@gmail.com>
2023-11-03 14:33:57 +0100

    dropped columns

--- Exploratory_data_analysis.py
+++ Exploratory_data_analysis.py
@@ -1,2 +1,13 @@
-from Data_preprocessing.py import *
+from Data_preprocessing import *
 df=preprocessing()
+
+# percentage of null values in all columns
+null_percentage = df.isnull().sum().sort_values(ascending = False)/df.shape[0]*100
+null_percentage
+
+# List of columns having more than 50% null values
+col_to_drop = null_percentage[null_percentage>= 50].keys()
+col_to_drop
+
+# drop the selected columns
+df = df.drop(col_to_drop, axis = 1)
