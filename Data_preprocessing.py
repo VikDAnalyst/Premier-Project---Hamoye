ed52cba0594c688b84db52eb651348de2d31821b
Elohor <okpakoelohor53@gmail.com>
2023-11-09 21:47:14 +0100

    modified Data preprocessing file

--- Data_preprocessing.py
+++ Data_preprocessing.py
@@ -911,25 +911,21 @@ def preprocessing():
     #sum of all taxes to be paid
     total_tax_paid= df[df.columns[df.columns.str.contains('tax')]].sum(axis=1)
     #equivalent columns for total tax
-    total_tax_paid
     #drop irrelevant columns of tax
     df.drop(['taxincome', 'taxprop','taxsales','taxother'], axis= 1, inplace= True)
     #include the equivalent tax column to the dataframe
     df['total_tax_paid']= total_tax_paid
-    df['total_tax_paid']
     #For the income section
     #Total income from farm activities per household will be the average of the average income yearly and the income for the last     12 monts
-    inc_farm= df[['incfarm', 'incfarm_n']].mean(axis=1)
+    #inc_farm= df[['incfarm', 'incfarm_n']].mean(axis=1)
     #Drop the irrelevant columns
-    df.drop(['incfarm', 'incfarm'], axis=1, inplace= True)
+    df.drop(['incfarm_n'], axis=1, inplace= True)
     #include the relevant column
-    df['inc_farm']= inc_farm
-    df['inc_farm']
+    #df['inc_farm']= inc_farm
     #Do the same for income from non farm activities
-    inc_nfarm= df[['incnfarm', 'incnfarm_n']].mean(axis=1)
+    #inc_nfarm= df[['incnfarm', 'incnfarm_n']].mean(axis=1)
     df.drop(['incnfarm', 'incnfarm_n'], axis=1, inplace= True)
-    df['inc_nfarm']= inc_nfarm
-    df['inc_nfarm']
+    #df['inc_nfarm']= inc_nfarm
     #The subsidy section
     df[df.columns[df.columns.str.contains('subs')]]
     df.drop(['subs1from', 'subs1get', 'subs1tot', 
